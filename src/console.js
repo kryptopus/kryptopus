035ebc565f00fb3f5de4f00f6fb15a69681c0d26
EC2 Default User <ec2-user@ip-10-0-0-246.eu-west-1.compute.internal>
2018-04-28 07:45:14 +0000

    Add architecture schema

--- src/console.js
+++ src/console.js
@@ -1,16 +1,8 @@
 import solfege from "solfegejs"
-import ExchangeBundle from "kryptopus-exchange"
-import BitstampBundle from "kryptopus-exchange-bitstamp"
-import TerminalInterfaceBundle from "kryptopus-terminal-interface"
-import TerminalInterfaceBodyBinanceExchangeBundle from "kryptopus-terminal-interface-body-binance-exchange"
 import KryptopusBundle from "./Bundle"
 
 // Create application instance
 let application = solfege.factory(`${__dirname}/../config/parameters.yml`);
-application.addBundle(new ExchangeBundle);
-application.addBundle(new BitstampBundle);
-application.addBundle(new TerminalInterfaceBundle);
-application.addBundle(new TerminalInterfaceBodyBinanceExchangeBundle);
 application.addBundle(new KryptopusBundle);
 
 
